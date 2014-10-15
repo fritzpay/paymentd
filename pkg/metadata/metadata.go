312954b5345560993a2296d606d38d9d760d83a6
seong <kang.seongmin@gmail.com>
2014-10-15 18:41:37 +0200

    metadata docs, get specific entry, generalize (might use different backends)

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -1,85 +1,27 @@
 package metadata
 
 import (
-	"database/sql"
-	"fmt"
 	"time"
 )
 
+// MetadataModel describes the concrete metadata model
 type MetadataModel interface {
 	Schema() string
 	PrimaryField() string
 }
+
+// MetadataEntry represents an entry in metadata
 type MetadataEntry struct {
 	Name      string
 	Timestamp time.Time
 	CreatedBy string
 	Value     string
 }
+
+// IsEmpty returns true if this is an empty entry
+func (e MetadataEntry) IsEmpty() bool {
+	return e.Name == ""
+}
+
+// Metadata is a collection of metadata entries
 type Metadata map[string]MetadataEntry
-
-const metadataByPrimary = `
-SELECT
-	md.name,
-	md.timestamp,
-	md.created_by,
-	md.value
-FROM %[1]s AS m
-WHERE
-	m.%[2]s = ?
-	AND
-	m.timestamp = (
-		SELECT MAX(timestamp) FROM %[1]s
-		WHERE
-			%[2]s = m.%[2]s
-			AND
-			name = m.name
-	)
-`
-
-func MetadataByPrimaryDB(db *sql.DB, m MetadataModel, primary int64) (Metadata, error) {
-	query := fmt.Sprintf(metadataByPrimary, m.Schema(), m.PrimaryField())
-	rows, err := db.Query(query, primary)
-	if err != nil {
-		return nil, err
-	}
-	metadata := Metadata(make(map[string]MetadataEntry))
-	var t int64
-	var e MetadataEntry
-	for rows.Next() {
-		err = rows.Scan(&e.Name, &t, &e.CreatedBy, &e.Value)
-		if err != nil {
-			rows.Close()
-			return nil, err
-		}
-		e.Timestamp = time.Unix(0, t)
-		metadata[e.Name] = e
-	}
-	err = rows.Err()
-	rows.Close()
-	return metadata, err
-}
-
-const insertMetadata = `
-INSERT INTO %s
-(%s, name, timestamp, created_by, value)
-VALUES
-(?, ?, ?, ?, ?)
-`
-
-func InsertMetadataTx(db *sql.Tx, m MetadataModel, primary int64, metadata Metadata) error {
-	insert, err := db.Prepare(fmt.Sprintf(insertMetadata, m.Schema(), m.PrimaryField()))
-	if err != nil {
-		return err
-	}
-	t := time.Now()
-	for _, e := range metadata {
-		_, err = insert.Exec(primary, e.Name, t.UnixNano(), e.CreatedBy, e.Value)
-		if err != nil {
-			insert.Close()
-			return err
-		}
-	}
-	insert.Close()
-	return nil
-}
---
+++ pkg/metadata/sql.go
@@ -0,0 +1,110 @@
+package metadata
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+const metadataFields = `
+SELECT
+	md.name,
+	md.timestamp,
+	md.created_by,
+	md.value
+`
+
+const metadataByPrimary = metadataFields + `
+FROM %[1]s AS m
+WHERE
+	m.%[2]s = ?
+	AND
+	m.timestamp = (
+		SELECT MAX(timestamp) FROM %[1]s
+		WHERE
+			%[2]s = m.%[2]s
+			AND
+			name = m.name
+	)
+`
+
+func MetadataByPrimaryDB(db *sql.DB, m MetadataModel, primary int64) (Metadata, error) {
+	query := fmt.Sprintf(metadataByPrimary, m.Schema(), m.PrimaryField())
+	rows, err := db.Query(query, primary)
+	if err != nil {
+		return nil, err
+	}
+	metadata := Metadata(make(map[string]MetadataEntry))
+	var t int64
+	var e MetadataEntry
+	for rows.Next() {
+		err = rows.Scan(&e.Name, &t, &e.CreatedBy, &e.Value)
+		if err != nil {
+			rows.Close()
+			return nil, err
+		}
+		e.Timestamp = time.Unix(0, t)
+		metadata[e.Name] = e
+	}
+	err = rows.Err()
+	rows.Close()
+	return metadata, err
+}
+
+const metadataByPrimaryAndName = metadataFields + `
+FROM %[1]s AS m
+WHERE
+	m.%[2]s = ?
+	AND
+	m.name = ?
+	m.timestamp = (
+		SELECT MAX(timestamp) FROM %[1]s
+		WHERE
+			%[2]s = m.%[2]s
+			AND
+			name = m.name
+	)
+`
+
+// MetadataByPrimaryAndNameDB selects a specific metadata entry
+//
+// If no such entry with the name exists, it will return an empty entry
+func MetadataByPrimaryAndNameDB(db *sql.DB, m MetadataModel, primary int64, name string) (MetadataEntry, error) {
+	query := fmt.Sprintf(metadataByPrimaryAndName, m.Schema(), m.PrimaryField())
+	row := db.QueryRow(query, primary, name)
+	var e MetadataEntry
+	var ts int64
+	err := row.Scan(&e.Name, &ts, &e.CreatedBy, &e.Value)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return e, nil
+		}
+		return e, err
+	}
+	e.Timestamp = time.Unix(0, ts)
+	return e, nil
+}
+
+const insertMetadata = `
+INSERT INTO %s
+(%s, name, timestamp, created_by, value)
+VALUES
+(?, ?, ?, ?, ?)
+`
+
+func InsertMetadataTx(db *sql.Tx, m MetadataModel, primary int64, metadata Metadata) error {
+	insert, err := db.Prepare(fmt.Sprintf(insertMetadata, m.Schema(), m.PrimaryField()))
+	if err != nil {
+		return err
+	}
+	t := time.Now()
+	for _, e := range metadata {
+		_, err = insert.Exec(primary, e.Name, t.UnixNano(), e.CreatedBy, e.Value)
+		if err != nil {
+			insert.Close()
+			return err
+		}
+	}
+	insert.Close()
+	return nil
+}
