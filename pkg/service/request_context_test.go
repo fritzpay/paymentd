8bed05ed8e3b8dbff5a16ee8939501c35257547e
seong <kang.seongmin@gmail.com>
2014-10-17 10:07:55 +0200

    remove request context timeout "hack", require to manually call ClearRequestContext

--- pkg/service/request_context_test.go
+++ pkg/service/request_context_test.go
@@ -4,45 +4,12 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 	"net/http"
 	"testing"
-	"time"
 )
 
 func TestRequestContextPurging(t *testing.T) {
 	Convey("Given a new request", t, WithContext(func(ctx *Context) {
 		r, err := http.NewRequest("GET", "www.example.com", nil)
 		So(err, ShouldBeNil)
-
-		Convey("Given a short timeout", func() {
-			oldT := RequestContextTimeout
-			RequestContextTimeout = func() time.Duration {
-				return 100 * time.Millisecond
-			}
-			Reset(func() {
-				RequestContextTimeout = oldT
-			})
-
-			Convey("When registering the context", func() {
-				SetRequestContext(r, ctx)
-				SetRequestContextVar(r, "test", "testvalue")
-				Convey("It should be returned", func() {
-					registeredCtx := RequestContext(r)
-					So(registeredCtx, ShouldNotBeNil)
-					testVal, ok := registeredCtx.Value("test").(string)
-					So(ok, ShouldBeTrue)
-					So(testVal, ShouldEqual, "testvalue")
-				})
-
-				Convey("When waiting for the timeout", func() {
-					time.Sleep(110 * time.Millisecond)
-					Convey("When retrieving the context", func() {
-						registeredCtx := RequestContext(r)
-						Convey("It should have been purged", func() {
-							So(registeredCtx, ShouldBeNil)
-						})
-					})
-				})
-			})
-		})
 
 		Convey("When registering the context", func() {
 			SetRequestContext(r, ctx)
