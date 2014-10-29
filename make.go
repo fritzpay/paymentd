19d6153530cda3628d288741d38f5f47c0c83153
seong <kang.seongmin@gmail.com>
2014-10-29 10:16:58 +0100

    hack something up for go

--- make.go
+++ make.go
@@ -10,10 +10,40 @@ This program builds paymentd
 package main
 
 import (
+	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	args := []string{}
+	log.SetFlags(0)
+	args := []string{
+		"get",
+		"-v",
+	}
+	pkgs := []string{
+		"code.google.com/p/go.tools/cmd/vet",
+		"github.com/tools/godep",
+	}
+	for _, pkg := range pkgs {
+		a := append(args, pkg)
+		cmd := exec.Command("go", a...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			os.Exit(1)
+		}
+	}
+	args = []string{
+		"vet",
+		"./...",
+	}
 	cmd := exec.Command("go", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		os.Exit(1)
+	}
 }
