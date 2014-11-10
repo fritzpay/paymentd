68376c23c0db7bbce7b3ffd290855a07303d1365
seong <kang.seongmin@gmail.com>
2014-11-10 18:00:06 +0100

    test fixtures with valid project key secret

--- resources/mysql/paymentd.test.principal.data.sql
+++ resources/mysql/paymentd.test.principal.data.sql
@@ -5,6 +5,6 @@ INSERT INTO `principal` (`id`, `created`, `created_by`, `name`)
 INSERT INTO `project` (`id`, `principal_id`, `name`, `created`, `created_by`) 
 	VALUES (1, 1, 'testproject', UTC_TIMESTAMP(), 'test');
 INSERT INTO `project_key` (`key`, `timestamp`, `project_id`, `created_by`, `secret`, `active`)
-	VALUES ('testkey', UTC_TIMESTAMP, 1, 'test', 'testsecret', 1);
+	VALUES ('testkey', UTC_TIMESTAMP, 1, 'test', 'abcdef', 1);
 
 COMMIT;
