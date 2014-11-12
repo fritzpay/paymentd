cee22ce439f84617d37989bef728b97463b18885
seong <kang.seongmin@gmail.com>
2014-11-12 13:36:01 +0100

    paypal tx tests

--- pkg/service/provider/paypal_rest/transaction_sql_test.go
+++ pkg/service/provider/paypal_rest/transaction_sql_test.go
@@ -3,6 +3,11 @@ package paypal_rest_test
 import (
 	"database/sql"
 	"testing"
+	"time"
+
+	"github.com/fritzpay/paymentd/pkg/paymentd/payment"
+
+	"github.com/fritzpay/paymentd/pkg/service/provider/paypal_rest"
 
 	"github.com/fritzpay/paymentd/pkg/testutil"
 	. "github.com/smartystreets/goconvey/convey"
@@ -17,6 +22,67 @@ func TestPaypalTransaction(t *testing.T) {
 				err = tx.Rollback()
 				So(err, ShouldBeNil)
 			})
+
+			Convey("Given a payment", testutil.WithPaymentInTx(tx, func(p *payment.Payment) {
+
+				Convey("When creating a new transaction", func() {
+					pt := &paypal_rest.Transaction{
+						ProjectID: p.ProjectID(),
+						PaymentID: p.ID(),
+						Timestamp: time.Now(),
+						Type:      "test",
+					}
+
+					Convey("When no paypal update time is set", func() {
+						So(pt.PaypalUpdateTime, ShouldBeNil)
+
+						Convey("When inserting the transaction", func() {
+							err = paypal_rest.InsertTransaction(tx, pt)
+
+							Convey("It should succeed", func() {
+								So(err, ShouldBeNil)
+
+								Convey("When retrieving the inserted transaction", func() {
+									ptRet, err := paypal_rest.TransactionCurrentByPaymentIDTx(tx, p.PaymentID())
+
+									Convey("It should succeed", func() {
+										So(err, ShouldBeNil)
+										So(ptRet, ShouldNotBeNil)
+										Convey("It should have no update time", func() {
+											So(ptRet.PaypalUpdateTime, ShouldBeNil)
+										})
+									})
+								})
+							})
+						})
+					})
+
+					Convey("When an update time is set", func() {
+						tm := time.Now()
+						pt.PaypalUpdateTime = &tm
+
+						Convey("When inserting the transaction", func() {
+							err = paypal_rest.InsertTransaction(tx, pt)
+
+							Convey("It should succeed", func() {
+								So(err, ShouldBeNil)
+
+								Convey("When retrieving the inserted transaction", func() {
+									ptRet, err := paypal_rest.TransactionCurrentByPaymentIDTx(tx, p.PaymentID())
+
+									Convey("It should succeed", func() {
+										So(err, ShouldBeNil)
+										So(ptRet, ShouldNotBeNil)
+										Convey("It should have an update time", func() {
+											So(ptRet.PaypalUpdateTime, ShouldNotBeNil)
+										})
+									})
+								})
+							})
+						})
+					})
+				})
+			}))
 		})
 	}))
 }
