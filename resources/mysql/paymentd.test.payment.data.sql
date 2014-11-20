280f5860c5078c1555353c3d5307d6f327484592
seong <kang.seongmin@gmail.com>
2014-11-20 13:19:57 +0100

    schema change: provider identified by name only

--- resources/mysql/paymentd.test.payment.data.sql
+++ resources/mysql/paymentd.test.payment.data.sql
@@ -1,7 +1,7 @@
 START TRANSACTION;
 
-INSERT INTO `payment_method` (`id`, `project_id`, `provider_id`, `method_key`, `created`, `created_by`) 
-	VALUES (1, 1, 1, 'test', UTC_TIMESTAMP(), 'test');
+INSERT INTO `payment_method` (`id`, `project_id`, `provider`, `method_key`, `created`, `created_by`) 
+	VALUES (1, 1, 'fritzpay', 'test', UTC_TIMESTAMP(), 'test');
 INSERT INTO `payment_method_status` (`payment_method_id`, `timestamp`, `created_by`, `status`)
 	VALUES (1, 1, 'test', 'active');
 	
