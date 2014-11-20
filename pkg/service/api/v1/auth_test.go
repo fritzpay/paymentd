a1424d7c7d702dac0ff5ae61bde55b2c83708c82
seong <kang.seongmin@gmail.com>
2014-11-20 13:59:34 +0100

    rework API tests

--- pkg/service/api/v1/auth_test.go
+++ pkg/service/api/v1/auth_test.go
@@ -4,32 +4,33 @@ import (
 	"database/sql"
 	"encoding/base64"
 	"encoding/json"
-	"net/http"
-	"testing"
-
-	"github.com/fritzpay/paymentd/pkg/paymentd/config"
 	"github.com/fritzpay/paymentd/pkg/service"
 	"github.com/fritzpay/paymentd/pkg/testutil"
 	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 	"gopkg.in/inconshreveable/log15.v2"
+	"net/http"
+	"testing"
 )
 
-func WithSystemPassword(db *sql.DB, f func()) func() {
+func WithAuthorization(mx *mux.Router, f func(auth string)) func() {
 	return func() {
-		cfg := config.NewConfig()
-		pw := config.SetPassword([]byte("password"))
-		err := pw(cfg)
+		req, err := http.NewRequest("GET", ServicePath+"/authorization/basic", nil)
+		So(err, ShouldBeNil)
+		req.Header.Set("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte("root:password")))
+
+		w := testutil.NewResponseWriter()
+		mx.ServeHTTP(w, req)
+
+		So(w.HeaderWritten, ShouldBeTrue)
+		So(w.StatusCode, ShouldEqual, http.StatusOK)
+
+		m := make(map[string]string)
+		dec := json.NewDecoder(&w.Buf)
+		err = dec.Decode(&m)
 		So(err, ShouldBeNil)
 
-		tx, err := db.Begin()
-		So(err, ShouldBeNil)
-		err = config.InsertConfigIfNotPresentTx(tx, cfg)
-		So(err, ShouldBeNil)
-		err = tx.Commit()
-		So(err, ShouldBeNil)
-
-		f()
+		f(m["Authorization"])
 	}
 }
 
@@ -77,98 +78,96 @@ func TestGetCredentialsWithBasicAuth(t *testing.T) {
 						db.Close()
 					})
 
-					Convey("Given a set system password", WithSystemPassword(db, func() {
-						Convey("When retrieving a basic authorization", func() {
-							r.Method = "GET"
-							r.URL.Path += "/basic"
+					Convey("When retrieving a basic authorization", func() {
+						r.Method = "GET"
+						r.URL.Path += "/basic"
 
-							Convey("When using the correct password", func() {
-								r.Header.Set("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte("root:password")))
+						Convey("When using the correct password", func() {
+							r.Header.Set("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte("root:password")))
 
-								Convey("When the handler is called", func() {
-									w := testutil.NewResponseWriter()
-									mx.ServeHTTP(w, r)
-									Convey("The handler should respond with OK", func() {
-										So(w.HeaderWritten, ShouldBeTrue)
-										So(w.StatusCode, ShouldEqual, http.StatusOK)
-										Convey("The body should contain the authorization container", func() {
-											m := make(map[string]string)
-											dec := json.NewDecoder(&w.Buf)
-											err := dec.Decode(&m)
-											So(err, ShouldBeNil)
-											So(m["Authorization"], ShouldNotBeEmpty)
-										})
-									})
-
-									Convey("Given the returned (correct) authorization container", func() {
+							Convey("When the handler is called", func() {
+								w := testutil.NewResponseWriter()
+								mx.ServeHTTP(w, r)
+								Convey("The handler should respond with OK", func() {
+									So(w.HeaderWritten, ShouldBeTrue)
+									So(w.StatusCode, ShouldEqual, http.StatusOK)
+									Convey("The body should contain the authorization container", func() {
 										m := make(map[string]string)
 										dec := json.NewDecoder(&w.Buf)
 										err := dec.Decode(&m)
 										So(err, ShouldBeNil)
 										So(m["Authorization"], ShouldNotBeEmpty)
+									})
+								})
 
-										Convey("Given a service request context", func() {
-											service.SetRequestContext(r, ctx)
+								Convey("Given the returned (correct) authorization container", func() {
+									m := make(map[string]string)
+									dec := json.NewDecoder(&w.Buf)
+									err := dec.Decode(&m)
+									So(err, ShouldBeNil)
+									So(m["Authorization"], ShouldNotBeEmpty)
 
-											Convey("Given a get user request", func() {
-												r.Method = "GET"
-												r.URL.Path = ServicePath + "/user"
-												r.Header.Set("Authorization", m["Authorization"])
+									Convey("Given a service request context", func() {
+										service.SetRequestContext(r, ctx)
 
-												Convey("When the handler is called", func() {
-													w := testutil.NewResponseWriter()
-													mx.ServeHTTP(w, r)
-													Convey("The handler should respond with OK", func() {
-														So(w.HeaderWritten, ShouldBeTrue)
-														So(w.StatusCode, ShouldEqual, http.StatusOK)
-													})
+										Convey("Given a get user request", func() {
+											r.Method = "GET"
+											r.URL.Path = ServicePath + "/user"
+											r.Header.Set("Authorization", m["Authorization"])
+
+											Convey("When the handler is called", func() {
+												w := testutil.NewResponseWriter()
+												mx.ServeHTTP(w, r)
+												Convey("The handler should respond with OK", func() {
+													So(w.HeaderWritten, ShouldBeTrue)
+													So(w.StatusCode, ShouldEqual, http.StatusOK)
 												})
 											})
 										})
 									})
 								})
-
-								Convey("Given cookie auth is allowed", func() {
-									ctx.Config().API.Cookie.AllowCookieAuth = true
-
-									Convey("When the handler is called", func() {
-										w := testutil.NewResponseWriter()
-										mx.ServeHTTP(w, r)
-										Convey("The handler should set a cookie", func() {
-											So(w.Header().Get("Set-Cookie"), ShouldNotBeEmpty)
-										})
-									})
-								})
 							})
 
-							Convey("When using a wrong password", func() {
-								r.Header.Set("Authorization", "Basic dede")
+							Convey("Given cookie auth is allowed", func() {
+								ctx.Config().API.Cookie.AllowCookieAuth = true
 
 								Convey("When the handler is called", func() {
 									w := testutil.NewResponseWriter()
 									mx.ServeHTTP(w, r)
-									Convey("The handler should respond with Unauthorized", func() {
-										So(w.HeaderWritten, ShouldBeTrue)
-										So(w.StatusCode, ShouldEqual, http.StatusUnauthorized)
-									})
-								})
-							})
-
-							Convey("When using a bad authorization header", func() {
-								r.Header.Set("Authorization", "Basic")
-
-								Convey("When the handler is called", func() {
-									w := testutil.NewResponseWriter()
-									mx.ServeHTTP(w, r)
-									Convey("The handler should request an authorization", func() {
-										So(w.HeaderWritten, ShouldBeTrue)
-										So(w.StatusCode, ShouldEqual, http.StatusUnauthorized)
-										So(w.Header().Get("WWW-Authenticate"), ShouldNotBeEmpty)
+									Convey("The handler should set a cookie", func() {
+										So(w.Header().Get("Set-Cookie"), ShouldNotBeEmpty)
 									})
 								})
 							})
 						})
-					}))
+
+						Convey("When using a wrong password", func() {
+							r.Header.Set("Authorization", "Basic dede")
+
+							Convey("When the handler is called", func() {
+								w := testutil.NewResponseWriter()
+								mx.ServeHTTP(w, r)
+								Convey("The handler should respond with Unauthorized", func() {
+									So(w.HeaderWritten, ShouldBeTrue)
+									So(w.StatusCode, ShouldEqual, http.StatusUnauthorized)
+								})
+							})
+						})
+
+						Convey("When using a bad authorization header", func() {
+							r.Header.Set("Authorization", "Basic")
+
+							Convey("When the handler is called", func() {
+								w := testutil.NewResponseWriter()
+								mx.ServeHTTP(w, r)
+								Convey("The handler should request an authorization", func() {
+									So(w.HeaderWritten, ShouldBeTrue)
+									So(w.StatusCode, ShouldEqual, http.StatusUnauthorized)
+									So(w.Header().Get("WWW-Authenticate"), ShouldNotBeEmpty)
+								})
+							})
+						})
+					})
 				}))
 			})
 		}))
