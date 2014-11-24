eff8a1068f64de40a2a74ec87cdaca81fc761957
seong <kang.seongmin@gmail.com>
2014-11-24 14:55:17 +0100

    web response writer sync

--- pkg/service/web/response.go
+++ pkg/service/web/response.go
@@ -2,24 +2,34 @@ package web
 
 import (
 	"net/http"
+	"sync"
 )
 
 type ResponseWriter struct {
 	http.ResponseWriter
 
+	mu             sync.Mutex
 	HTTPStatusCode int
 	HeaderWritten  bool
 	Written        int
 }
 
 func (r *ResponseWriter) WriteHeader(s int) {
+	r.mu.Lock()
+	if r.HeaderWritten {
+		r.mu.Unlock()
+		return
+	}
 	r.HTTPStatusCode = s
 	r.HeaderWritten = true
+	r.mu.Unlock()
 	r.ResponseWriter.WriteHeader(s)
 }
 
 func (r *ResponseWriter) Write(b []byte) (int, error) {
 	w, err := r.ResponseWriter.Write(b)
+	r.mu.Lock()
 	r.Written += w
+	r.mu.Unlock()
 	return w, err
 }
