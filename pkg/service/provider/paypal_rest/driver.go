f8f9706ddb136289e15f13f0a78a1e50775b22fc
seong <kang.seongmin@gmail.com>
2014-11-27 12:46:58 +0100

    refactor: join more files

--- pkg/service/provider/paypal_rest/driver.go
+++ pkg/service/provider/paypal_rest/driver.go
@@ -1,6 +1,7 @@
 package paypal_rest
 
 import (
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -9,7 +10,10 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"strings"
 	"time"
+
+	"github.com/fritzpay/paymentd/pkg/paymentd/nonce"
 
 	"github.com/fritzpay/paymentd/pkg/paymentd/payment_method"
 
@@ -32,6 +36,8 @@ const (
 const (
 	providerTemplateDir = "paypal_rest"
 	defaultLocale       = "en_US"
+	// endpoint path for REST API URL
+	paypalPaymentPath = "/v1/payments/payment"
 )
 
 var (
@@ -248,4 +254,590 @@ func (d *Driver) getPayment(p *payment.Payment) {
 	if err != nil {
 		log.Error("error on executing HTTP request", log15.Ctx{"err": err})
 	}
+}
+
+func (d *Driver) InitPayment(p *payment.Payment, method *payment_method.Method) (http.Handler, error) {
+	log := d.log.New(log15.Ctx{
+		"method":          "InitPayment",
+		"projectID":       p.ProjectID(),
+		"paymentID":       p.ID(),
+		"paymentMethodID": method.ID,
+	})
+
+	var tx *sql.Tx
+	var err error
+	var commit bool
+	defer func() {
+		if tx != nil && !commit {
+			err = tx.Rollback()
+			if err != nil {
+				log.Crit("error on rollback", log15.Ctx{"err": err})
+			}
+		}
+	}()
+	tx, err = d.ctx.PaymentDB().Begin()
+	if err != nil {
+		commit = true
+		log.Crit("error on begin tx", log15.Ctx{"err": err})
+		return nil, ErrDatabase
+	}
+
+	currentTx, err := TransactionCurrentByPaymentIDTx(tx, p.PaymentID())
+	if err != nil && err != ErrTransactionNotFound {
+		log.Error("error retrieving transaction", log15.Ctx{"err": err})
+		return nil, ErrDatabase
+	}
+	if err == nil {
+		if Debug {
+			log.Debug("already initialized payment")
+		}
+		return d.statusHandler(currentTx, p, d.InitPageHandler(p)), nil
+	}
+
+	cfg, err := ConfigByPaymentMethodTx(tx, method)
+	if err != nil {
+		log.Error("error retrieving PayPal config", log15.Ctx{"err": err})
+		return nil, ErrDatabase
+	}
+
+	// create payment request
+	non, err := nonce.New()
+	if err != nil {
+		log.Error("error generating nonce", log15.Ctx{"err": err})
+		return nil, ErrInternal
+	}
+	req, err := d.createPaypalPaymentRequest(p, cfg, non)
+	if err != nil {
+		log.Error("error creating paypal payment request", log15.Ctx{"err": err})
+		return nil, ErrInternal
+	}
+	if Debug {
+		log.Debug("created paypal payment request", log15.Ctx{"request": req})
+	}
+
+	endpoint, err := url.Parse(cfg.Endpoint)
+	if err != nil {
+		log.Error("error on endpoint URL", log15.Ctx{"err": err})
+		return nil, ErrInternal
+	}
+	endpoint.Path = paypalPaymentPath
+
+	jsonBytes, err := json.Marshal(req)
+	if err != nil {
+		log.Error("error encoding request", log15.Ctx{"err": err})
+		return nil, ErrInternal
+	}
+
+	paypalTx := &Transaction{
+		ProjectID: p.ProjectID(),
+		PaymentID: p.ID(),
+		Timestamp: time.Now(),
+		Type:      TransactionTypeCreatePayment,
+	}
+	paypalTx.SetIntent(cfg.Type)
+	paypalTx.SetNonce(non.Nonce)
+	paypalTx.Data = jsonBytes
+
+	err = InsertTransactionTx(tx, paypalTx)
+	if err != nil {
+		log.Error("error saving transaction", log15.Ctx{"err": err})
+		return nil, ErrDatabase
+	}
+
+	commit = true
+	err = tx.Commit()
+	if err != nil {
+		log.Crit("error on commit", log15.Ctx{"err": err})
+		return nil, ErrDatabase
+	}
+
+	go d.doInit(cfg, endpoint, p, string(jsonBytes))
+
+	return d.statusHandler(currentTx, p, d.InitPageHandler(p)), nil
+}
+
+func (d *Driver) doInit(cfg *Config, reqURL *url.URL, p *payment.Payment, body string) {
+	log := d.log.New(log15.Ctx{
+		"method":      "doInit",
+		"projectID":   p.ProjectID(),
+		"paymentID":   p.ID(),
+		"methodKey":   cfg.MethodKey,
+		"requestBody": body,
+	})
+	if Debug {
+		log.Debug("posting...")
+	}
+
+	req, err := http.NewRequest("POST", reqURL.String(), strings.NewReader(body))
+	if err != nil {
+		log.Error("error creating HTTP request", log15.Ctx{"err": err})
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	responseFunc := func(resp *http.Response, err error) error {
+		if err != nil {
+			log.Error("error on HTTP", log15.Ctx{"err": err})
+			d.setPayPalError(p, nil)
+			return err
+		}
+		respBody, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			log.Error("error reading response body", log15.Ctx{"err": err})
+			d.setPayPalError(p, nil)
+			return ErrHTTP
+		}
+		log = log.New(log15.Ctx{"responseBody": string(respBody)})
+		if Debug {
+			log.Debug("received response")
+		}
+		if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusOK {
+			log.Error("error on HTTP request", log15.Ctx{"HTTPStatusCode": resp.StatusCode})
+			d.setPayPalError(p, respBody)
+			return ErrHTTP
+		}
+		paypalP := &PaypalPayment{}
+		err = json.Unmarshal(respBody, paypalP)
+		if err != nil {
+			log.Error("error decoding PayPal response", log15.Ctx{"err": err})
+			d.setPayPalError(p, respBody)
+			return ErrProvider
+		}
+
+		paypalTx, err := NewPayPalPaymentTransaction(paypalP)
+		if err != nil && paypalTx == nil {
+			log.Error("error on creating response transaction", log15.Ctx{"err": err})
+			d.setPayPalError(p, respBody)
+			return ErrProvider
+		}
+		if err != nil {
+			log.Warn("error on parsing response for transaction", log15.Ctx{"err": err})
+		}
+		paypalTx.ProjectID = p.ProjectID()
+		paypalTx.PaymentID = p.ID()
+		paypalTx.Type = TransactionTypeCreatePaymentResponse
+
+		var tx *sql.Tx
+		var commit bool
+		defer func() {
+			if tx != nil && !commit {
+				err = tx.Rollback()
+				if err != nil {
+					log.Crit("error on rollback", log15.Ctx{"err": err})
+				}
+			}
+		}()
+		tx, err = d.ctx.PaymentDB().Begin()
+		if err != nil {
+			log.Crit("error on creating db tx", log15.Ctx{"err": err})
+			d.setPayPalError(p, respBody)
+			return ErrDatabase
+		}
+		err = InsertTransactionTx(tx, paypalTx)
+		if err != nil {
+			log.Error("error saving paypal response", log15.Ctx{"err": err})
+			d.setPayPalError(p, respBody)
+			return ErrDatabase
+		}
+		if paypalP.State != "created" {
+			log.Error("invalid paypal state received", log15.Ctx{"state": paypalP.State})
+			paypalTx.Type = TransactionTypeError
+			paypalTx.Timestamp = time.Now()
+			err = InsertTransactionTx(tx, paypalTx)
+			if err != nil {
+				log.Error("error saving error state", log15.Ctx{"err": err})
+				d.setPayPalError(p, respBody)
+				return ErrDatabase
+			}
+		}
+
+		commit = true
+		err = tx.Commit()
+		if err != nil {
+			log.Crit("error on commit", log15.Ctx{"err": err})
+			d.setPayPalError(p, respBody)
+			return ErrDatabase
+		}
+		return nil
+	}
+
+	err = httpDo(d.ctx, d.oAuthTransportFunc(p, cfg), req, responseFunc)
+	if err != nil {
+		log.Error("error on create payment request", log15.Ctx{"err": err})
+	}
+}
+
+func (d *Driver) ReturnHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := d.log.New(log15.Ctx{"method": "ReturnHandler"})
+		paymentIDStr := r.URL.Query().Get(paymentIDParam)
+		if paymentIDStr == "" {
+			log.Info("request without payment ID")
+			d.NotFoundHandler(nil).ServeHTTP(w, r)
+			return
+		}
+		nonce := r.URL.Query().Get(nonceParam)
+		if nonce == "" {
+			log.Info("request without nonce")
+			d.NotFoundHandler(nil).ServeHTTP(w, r)
+		}
+		paymentID, err := payment.ParsePaymentIDStr(paymentIDStr)
+		if err != nil {
+			log.Warn("error parsing payment ID", log15.Ctx{
+				"err":          err,
+				"paymentIDStr": paymentIDStr,
+			})
+			d.BadRequestHandler().ServeHTTP(w, r)
+			return
+		}
+		paymentID = d.paymentService.DecodedPaymentID(paymentID)
+		payerID := r.URL.Query().Get(paypalPayerIDParameter)
+
+		var tx *sql.Tx
+		var commit bool
+		defer func() {
+			if tx != nil && !commit {
+				err = tx.Rollback()
+				if err != nil {
+					log.Crit("error on rollback", log15.Ctx{"err": err})
+				}
+			}
+		}()
+		tx, err = d.ctx.PaymentDB().Begin()
+		if err != nil {
+			commit = true
+			log.Crit("error on begin tx", log15.Ctx{"err": err})
+			d.InternalErrorHandler(nil).ServeHTTP(w, r)
+			return
+		}
+		_, err = TransactionByPaymentIDAndNonceTx(tx, paymentID, nonce)
+		if err != nil {
+			if err == ErrTransactionNotFound {
+				log.Info("paypal transaction not found")
+				d.NotFoundHandler(nil).ServeHTTP(w, r)
+				return
+			}
+			log.Error("error retrieving paypal transaction")
+			d.InternalErrorHandler(nil).ServeHTTP(w, r)
+			return
+		}
+		p, err := payment.PaymentByIDTx(tx, paymentID)
+		if err != nil {
+			if err == payment.ErrPaymentNotFound {
+				log.Info("payment not found", log15.Ctx{"err": err})
+				d.NotFoundHandler(nil).ServeHTTP(w, r)
+				return
+			}
+			log.Error("error retrieving payment", log15.Ctx{"err": err})
+			d.InternalErrorHandler(nil).ServeHTTP(w, r)
+			return
+		}
+		method, err := payment_method.PaymentMethodByIDTx(tx, p.Config.PaymentMethodID.Int64)
+		if err != nil {
+			log.Error("error retrieving payment method", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+		if !method.Active() {
+			log.Error("inactive payment method", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+		cfg, err := ConfigByPaymentMethodTx(tx, method)
+		if err != nil {
+			log.Error("error retrieving config", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+		currentTx, err := TransactionCurrentByPaymentIDTx(tx, p.PaymentID())
+		if err != nil {
+			log.Error("error retrieving current transaction", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+		if currentTx.Type != TransactionTypeCreatePaymentResponse {
+			if Debug {
+				log.Debug("no execute payment required. skipping...")
+			}
+			d.statusHandler(currentTx, p, d.ReturnPageHandler(p)).ServeHTTP(w, r)
+			return
+		}
+
+		exec := &PayPalPaymentExecution{
+			PayerID: payerID,
+		}
+		execJSON, err := json.Marshal(exec)
+		if err != nil {
+			log.Error("error encoding execute payment request", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+
+		if currentTx.Links == nil {
+			log.Crit("create payment response without links")
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+		links, err := currentTx.PayPalLinks()
+		if err != nil {
+			log.Error("error retrieving links", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+		if links["execute"] == nil {
+			log.Error("no execute link", log15.Ctx{"links": links})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+		execURL, err := url.Parse(links["execute"].HRef)
+		if err != nil {
+			log.Error("error parsing execute URL", log15.Ctx{"err": err, "execURL": links["execute"].HRef})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+		execTx := &Transaction{
+			ProjectID: p.ProjectID(),
+			PaymentID: p.ID(),
+			Timestamp: time.Now(),
+			Type:      TransactionTypeExecutePayment,
+			Links:     currentTx.Links,
+			Data:      execJSON,
+		}
+		execTx.SetPayerID(payerID)
+		if currentTx.PaypalID.Valid {
+			currentTx.SetPaypalID(currentTx.PaypalID.String)
+		}
+		err = InsertTransactionTx(tx, execTx)
+		if err != nil {
+			log.Error("error saving execute payment transaction", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+
+		commit = true
+		err = tx.Commit()
+		if err != nil {
+			log.Crit("error on commit tx", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+
+		go d.executePayment(cfg, execURL, p, string(execJSON))
+
+		d.statusHandler(execTx, p, d.ReturnPageHandler(p)).ServeHTTP(w, r)
+	})
+}
+
+func (d *Driver) executePayment(cfg *Config, reqURL *url.URL, p *payment.Payment, body string) {
+	log := d.log.New(log15.Ctx{
+		"method":    "executePayment",
+		"projectID": p.ProjectID(),
+		"paymentID": p.ID(),
+		"body":      body,
+	})
+	log.Debug("executing payment...")
+
+	paymentTx, commitIntent, err := d.paymentService.IntentPaid(p, 500*time.Millisecond)
+	if err != nil {
+		log.Error("error on intent paid", log15.Ctx{"err": err})
+		d.setPayPalError(p, nil)
+		return
+	}
+
+	req, err := http.NewRequest("POST", reqURL.String(), strings.NewReader(body))
+	if err != nil {
+		log.Error("error creating execute payment request", log15.Ctx{"err": err})
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	responseFunc := func(resp *http.Response, err error) error {
+		if err != nil {
+			log.Error("error on request", log15.Ctx{"err": err})
+			return err
+		}
+		respBody, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			log.Error("error reading response body", log15.Ctx{"err": err})
+			d.setPayPalError(p, nil)
+			return ErrHTTP
+		}
+		log = log.New(log15.Ctx{"responseBody": string(respBody)})
+		if resp.StatusCode != http.StatusOK {
+			log.Error("invalid HTTP status code", log15.Ctx{"statusCode": resp.StatusCode})
+			d.setPayPalError(p, respBody)
+			return ErrHTTP
+		}
+		if Debug {
+			log.Debug("received response")
+		}
+		pay := &PaypalPayment{}
+		err = json.Unmarshal(respBody, pay)
+		if err != nil {
+			log.Error("error decoding response", log15.Ctx{"err": err})
+			d.setPayPalError(p, respBody)
+			return ErrHTTP
+		}
+		paypalTx, err := NewPayPalPaymentTransaction(pay)
+		if err != nil && paypalTx == nil {
+			log.Error("error creating response transaction", log15.Ctx{"err": err})
+			d.setPayPalError(p, respBody)
+			return ErrInternal
+		}
+		if err != nil {
+			log.Warn("error parsing response", log15.Ctx{"err": err})
+		}
+		paypalTx.ProjectID = p.ProjectID()
+		paypalTx.PaymentID = p.ID()
+		paypalTx.Type = TransactionTypeExecutePaymentResponse
+
+		var tx *sql.Tx
+		var commit bool
+		defer func() {
+			if tx != nil && !commit {
+				err = tx.Rollback()
+				if err != nil {
+					log.Crit("error on rollback", log15.Ctx{"err": err})
+				}
+			}
+		}()
+		tx, err = d.ctx.PaymentDB().Begin()
+		if err != nil {
+			log.Crit("error on begin tx", log15.Ctx{"err": err})
+			return ErrDatabase
+		}
+
+		err = InsertTransactionTx(tx, paypalTx)
+		if err != nil {
+			log.Error("error saving paypal transaction", log15.Ctx{"err": err})
+			return ErrDatabase
+		}
+
+		paymentTx.Comment.String, paymentTx.Comment.Valid = "PayPal PaymentID: "+pay.ID, true
+		err = d.paymentService.SetPaymentTransaction(tx, paymentTx)
+		if err != nil {
+			log.Error("error on payment transaction", log15.Ctx{"err": err})
+			return ErrDatabase
+		}
+
+		commit = true
+		err = tx.Commit()
+		if err != nil {
+			log.Crit("error on commit", log15.Ctx{"err": err})
+			return ErrDatabase
+		}
+		commitIntent()
+
+		return nil
+	}
+	err = httpDo(d.ctx, d.oAuthTransportFunc(p, cfg), req, responseFunc)
+	if err != nil {
+		log.Error("error on executing HTTP request", log15.Ctx{"err": err})
+	}
+}
+
+func (d *Driver) CancelHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := d.log.New(log15.Ctx{"method": "CancelHandler"})
+		paymentIDStr := r.URL.Query().Get(paymentIDParam)
+		if paymentIDStr == "" {
+			log.Info("request without payment ID")
+			d.NotFoundHandler(nil).ServeHTTP(w, r)
+			return
+		}
+		nonce := r.URL.Query().Get(nonceParam)
+		if nonce == "" {
+			log.Info("request without nonce")
+			d.NotFoundHandler(nil).ServeHTTP(w, r)
+		}
+		paymentID, err := payment.ParsePaymentIDStr(paymentIDStr)
+		if err != nil {
+			log.Warn("error parsing payment ID", log15.Ctx{
+				"err":          err,
+				"paymentIDStr": paymentIDStr,
+			})
+			d.BadRequestHandler().ServeHTTP(w, r)
+			return
+		}
+		paymentID = d.paymentService.DecodedPaymentID(paymentID)
+
+		var tx *sql.Tx
+		var commit bool
+		defer func() {
+			if tx != nil && !commit {
+				err = tx.Rollback()
+				if err != nil {
+					log.Crit("error on rollback", log15.Ctx{"err": err})
+				}
+			}
+		}()
+		tx, err = d.ctx.PaymentDB().Begin()
+		if err != nil {
+			commit = true
+			log.Crit("error on begin tx", log15.Ctx{"err": err})
+			d.InternalErrorHandler(nil).ServeHTTP(w, r)
+			return
+		}
+
+		_, err = TransactionByPaymentIDAndNonceTx(tx, paymentID, nonce)
+		if err != nil {
+			if err == ErrTransactionNotFound {
+				log.Info("paypal transaction not found")
+				d.NotFoundHandler(nil).ServeHTTP(w, r)
+				return
+			}
+			log.Error("error retrieving paypal transaction")
+			d.InternalErrorHandler(nil).ServeHTTP(w, r)
+			return
+		}
+		p, err := payment.PaymentByIDTx(tx, paymentID)
+		if err != nil {
+			if err == payment.ErrPaymentNotFound {
+				log.Info("payment not found", log15.Ctx{"err": err})
+				d.NotFoundHandler(nil).ServeHTTP(w, r)
+				return
+			}
+			log.Error("error retrieving payment", log15.Ctx{"err": err})
+			d.InternalErrorHandler(nil).ServeHTTP(w, r)
+			return
+		}
+
+		var paymentTx *payment.PaymentTransaction
+		var commitIntent paymentService.CommitIntentFunc
+		if p.Status != payment.PaymentStatusCancelled {
+			if Debug {
+				log.Debug("intent cancel")
+			}
+			paymentTx, commitIntent, err = d.paymentService.IntentCancel(p, 500*time.Millisecond)
+			if err != nil {
+				log.Error("error on intent payment cancel", log15.Ctx{"err": err})
+				d.PaymentErrorHandler(p).ServeHTTP(w, r)
+				return
+			}
+			err = d.paymentService.SetPaymentTransaction(tx, paymentTx)
+			if err != nil {
+				log.Error("error creating payment transaction", log15.Ctx{"err": err})
+				d.InternalErrorHandler(p).ServeHTTP(w, r)
+				return
+			}
+		}
+
+		commit = true
+		err = tx.Commit()
+		if err != nil {
+			log.Crit("error on commit", log15.Ctx{"err": err})
+			d.InternalErrorHandler(p).ServeHTTP(w, r)
+			return
+		}
+
+		// do notify on new payment tx
+		if commitIntent != nil {
+			if Debug {
+				log.Debug("intent commit", log15.Ctx{"commitIntent": commitIntent})
+			}
+			commitIntent()
+		}
+
+		d.CancelPageHandler(p).ServeHTTP(w, r)
+	})
 }
--- pkg/service/provider/paypal_rest/sql.go
+++ pkg/service/provider/paypal_rest/sql.go
@@ -6,11 +6,72 @@ import (
 	"time"
 
 	"github.com/fritzpay/paymentd/pkg/paymentd/payment"
+
+	"github.com/fritzpay/paymentd/pkg/paymentd/payment_method"
 )
 
 var (
+	ErrConfigNotFound      = errors.New("config not found")
 	ErrTransactionNotFound = errors.New("transaction not found")
 )
+
+const selectConfig = `
+SELECT
+	c.project_id,
+	c.method_key,
+	c.created,
+	c.created_by,
+	c.endpoint,
+	c.client_id,
+	c.secret,
+	c.type
+FROM provider_paypal_config AS c
+`
+const selectConfigByProjectIDAndMethodKey = selectConfig + `
+WHERE
+	c.project_id = ?
+	AND
+	c.method_key = ?
+	AND
+	c.created = (
+		SELECT MAX(created) FROM provider_paypal_config
+		WHERE
+			project_id = c.project_id
+			AND
+			method_key = c.method_key
+	)
+`
+
+func scanConfig(row *sql.Row) (*Config, error) {
+	cfg := &Config{}
+	err := row.Scan(
+		&cfg.ProjectID,
+		&cfg.MethodKey,
+		&cfg.Created,
+		&cfg.CreatedBy,
+		&cfg.Endpoint,
+		&cfg.ClientID,
+		&cfg.Secret,
+		&cfg.Type,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return cfg, ErrConfigNotFound
+		}
+		return cfg, err
+	}
+	return cfg, nil
+}
+
+func ConfigByPaymentMethodTx(db *sql.Tx, method *payment_method.Method) (*Config, error) {
+	row := db.QueryRow(selectConfigByProjectIDAndMethodKey, method.ProjectID, method.MethodKey)
+	return scanConfig(row)
+}
+
+func ConfigByPaymentMethodDB(db *sql.DB, method *payment_method.Method) (*Config, error) {
+	row := db.QueryRow(selectConfigByProjectIDAndMethodKey, method.ProjectID, method.MethodKey)
+	return scanConfig(row)
+}
 
 const selectTransaction = `
 SELECT
